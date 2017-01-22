93543961fde281e59ca2cc6244657c08c49b0403
John Keith <johnkeith@outlook.com>
2017-01-22 09:50:37 -0500

    Initial Commit

---
+++ GoTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoTime
+//
+//  Created by John Keith on 1/22/17.
+//  Copyright © 2017 John Keith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
