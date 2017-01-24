f0fdae223259720ac37724a6d0cc02970e6d5c51
John Keith <johnkeith@outlook.com>
2017-01-24 12:43:10 -0500

    working on first test

---
+++ GoTimeTests/MainViewControllerTests.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewControllerTests.swift
+//  GoTime
+//
+//  Created by John Keith on 1/23/17.
+//  Copyright © 2017 John Keith. All rights reserved.
+//
+
+import XCTest
+@testable import GoTime
+
+class MainViewControllerTests: XCTestCase {
+//    override func setUp() {
+//        super.setUp()
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let viewController = storyboard.instantiateViewController(withIdentifier :"MainViewController") as! UIViewController
+//    }
+//    
+//    override func tearDown() {
+//        super.tearDown()
+//    }    
+
+}
