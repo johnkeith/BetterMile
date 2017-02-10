56170c52d01a4c8dab5fe98e1288aa55348334fe
John Keith <johnkeith@outlook.com>
2017-02-09 19:28:45 -0500

    added lap time table view class

---
+++ GoTime/LapTimeTable.swift
@@ -0,0 +1,25 @@
+//
+//  LapTimeTable.swift
+//  GoTime
+//
+//  Created by John Keith on 2/8/17.
+//  Copyright © 2017 John Keith. All rights reserved.
+//
+
+import UIKit
+
+class LapTimeTable: UITableView {
+    init() {
+        let defaultFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        super.init(frame: defaultFrame, style: .plain)
+    }
+
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: style)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
