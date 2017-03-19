e12e6d2421047ecd3dc73edc600d5e06d8778cdb
John Keith <johnkeith@outlook.com>
2017-03-18 22:06:08 -0400

    add delegate and tap function for settings button

--- GoTime/SettingsButton.swift
+++ GoTime/SettingsButton.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+protocol SettingsButtonDelegate: class {
+    func onSettingsTap(sender: SettingsButton)
+}
+
 class SettingsButton: UIButton {
+    var delegate: SettingsButtonDelegate!
+    
     init() {
         let defaultFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
         let buttonImage = UIImage(named: "ic_more_horiz")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
@@ -18,10 +24,16 @@ class SettingsButton: UIButton {
         self.tintColor = Constants.colorPalette["black"]
         self.setImage(buttonImage, for: UIControlState.normal)
         self.contentHorizontalAlignment = UIControlContentHorizontalAlignment.right
+        
+        addTarget(self, action:#selector(onSettingsTap), for: .touchUpInside)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) is not supported")
+    }
+    
+    func onSettingsTap(sender: SettingsButton) {
+        delegate.onSettingsTap(sender: sender)
     }
     
     func hide() {
