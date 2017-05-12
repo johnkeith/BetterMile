38dfd678704d98151f73a563e5e1f11a9c406d97
John Keith <johnkeith@outlook.com>
2017-05-11 22:24:31 -0400

    all buttons showing and properly aligned

--- GoTime/SingleViewController.swift
+++ GoTime/SingleViewController.swift
@@ -14,6 +14,13 @@ class SingleViewController: UIViewController {
     let startBtn = UIButton()
     let totalTimeLbl = UILabel()
     let lapLbl = UILabel()
+    let voiceNotificationsBtn = UIButton()
+    let vibrationNotificationBtn = UIButton()
+    let pauseBtn = UIButton()
+    let clearBtn = UIButton()
+    let restartBtn = UIButton()
+    let lapTableBtn = UIButton()
+    
     var doubleTapRecognizer: UITapGestureRecognizer! // TODO: FIX
     
 //  DI
@@ -34,11 +41,17 @@ class SingleViewController: UIViewController {
         
         view.backgroundColor = Constants.colorPalette["white"]
         
-        addSubviews([startBtn, totalTimeLbl, lapLbl])
+        addSubviews([startBtn, totalTimeLbl, lapLbl, voiceNotificationsBtn, pauseBtn, vibrationNotificationBtn, clearBtn, restartBtn, lapTableBtn])
 
         configStartBtn()
         configTotalTimeLbl()
         configLapLbl()
+        configVoiceNotificationsBtn()
+        configPauseBtn()
+        configVibrationNotificationBtn()
+        configClearBtn()
+        configRestartBtn()
+        configLapTableBtn()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -77,7 +90,7 @@ class SingleViewController: UIViewController {
             make.width.equalTo(totalTimeLbl.superview!).offset(-Constants.defaultMargin)
             make.height.equalTo(totalTimeLbl.superview!.frame.height / 6)
             make.centerX.equalTo(totalTimeLbl.superview!)
-            make.top.equalTo(totalTimeLbl.superview!).offset(Constants.defaultMargin * 2)
+            make.top.equalTo(totalTimeLbl.superview!).offset(Constants.defaultMargin)
         }
     }
     
@@ -95,6 +108,149 @@ class SingleViewController: UIViewController {
         lapLbl.layoutIfNeeded()
         
         lapLbl.font = UIFont.systemFont(ofSize: lapLbl.frame.size.height, weight: UIFontWeightThin)
+    }
+    
+    func configVoiceNotificationsBtn() {
+        let buttonImage = UIImage(named: "ic_volume_up_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        voiceNotificationsBtn.isHidden = false
+        voiceNotificationsBtn.tintColor = Constants.colorPalette["white"]
+        voiceNotificationsBtn.backgroundColor = Constants.colorPalette["black"]
+        voiceNotificationsBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = voiceNotificationsBtn.superview!.frame.width / 5
+        
+        voiceNotificationsBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(voiceNotificationsBtn.snp.width)
+            make.bottom.equalTo(voiceNotificationsBtn.superview!).offset(-Constants.defaultMargin)
+            make.right.equalTo(voiceNotificationsBtn.superview!).offset(-Constants.defaultMargin)
+        }
+        
+        voiceNotificationsBtn.layer.borderWidth = 2.0
+        
+        voiceNotificationsBtn.layoutIfNeeded()
+        
+        voiceNotificationsBtn.layer.cornerRadius = voiceNotificationsBtn.frame.size.height / 2
+    }
+    
+    func configPauseBtn() {
+        let buttonImage = UIImage(named: "ic_pause_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        pauseBtn.isHidden = false
+        pauseBtn.tintColor = Constants.colorPalette["white"]
+        pauseBtn.backgroundColor = Constants.colorPalette["black"]
+        pauseBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = pauseBtn.superview!.frame.width / 5
+       
+        pauseBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(pauseBtn.snp.width)
+            make.bottom.equalTo(pauseBtn.superview!).offset(-Constants.defaultMargin)
+            make.centerX.equalTo(pauseBtn.superview!)
+        }
+        
+        pauseBtn.layer.borderWidth = 2.0
+        
+        pauseBtn.layoutIfNeeded()
+        
+        pauseBtn.layer.cornerRadius = pauseBtn.frame.size.height / 2
+    }
+    
+    func configVibrationNotificationBtn() {
+        let buttonImage = UIImage(named: "ic_vibration_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        vibrationNotificationBtn.isHidden = false
+        vibrationNotificationBtn.tintColor = Constants.colorPalette["white"]
+        vibrationNotificationBtn.backgroundColor = Constants.colorPalette["black"]
+        vibrationNotificationBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = vibrationNotificationBtn.superview!.frame.width / 5
+        vibrationNotificationBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(vibrationNotificationBtn.snp.width)
+            make.bottom.equalTo(vibrationNotificationBtn.superview!).offset(-Constants.defaultMargin)
+            make.left.equalTo(vibrationNotificationBtn.superview!).offset(Constants.defaultMargin)
+        }
+        
+        vibrationNotificationBtn.layer.borderWidth = 2.0
+        
+        vibrationNotificationBtn.layoutIfNeeded()
+        
+        vibrationNotificationBtn.layer.cornerRadius = vibrationNotificationBtn.frame.size.height / 2
+    }
+    
+    func configClearBtn() {
+        let buttonImage = UIImage(named: "ic_clear_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        clearBtn.isHidden = false
+        clearBtn.tintColor = Constants.colorPalette["white"]
+        clearBtn.backgroundColor = Constants.colorPalette["black"]
+        clearBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = clearBtn.superview!.frame.width / 5
+        
+        clearBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(clearBtn.snp.width)
+            make.bottom.equalTo(clearBtn.superview!).offset(-width - CGFloat(Constants.defaultMargin))
+            make.left.equalTo(clearBtn.superview!).offset(width + CGFloat(Constants.defaultMargin / 2))
+        }
+        
+        clearBtn.layer.borderWidth = 2.0
+        
+        clearBtn.layoutIfNeeded()
+        
+        clearBtn.layer.cornerRadius = clearBtn.frame.size.height / 2
+    }
+    
+    func configRestartBtn() {
+        let buttonImage = UIImage(named: "ic_play_arrow_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        restartBtn.isHidden = false
+        restartBtn.tintColor = Constants.colorPalette["white"]
+        restartBtn.backgroundColor = Constants.colorPalette["black"]
+        restartBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = restartBtn.superview!.frame.width / 5
+        
+        restartBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(restartBtn.snp.width)
+            make.bottom.equalTo(restartBtn.superview!).offset(-Constants.defaultMargin)
+            make.centerX.equalTo(restartBtn.superview!)
+        }
+        
+        restartBtn.layer.borderWidth = 2.0
+        
+        restartBtn.layoutIfNeeded()
+        
+        restartBtn.layer.cornerRadius = restartBtn.frame.size.height / 2
+    }
+    
+    func configLapTableBtn() {
+        let buttonImage = UIImage(named: "ic_format_list_numbered_48pt")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
+        
+        lapTableBtn.isHidden = false
+        lapTableBtn.tintColor = Constants.colorPalette["white"]
+        lapTableBtn.backgroundColor = Constants.colorPalette["black"]
+        lapTableBtn.setImage(buttonImage, for: UIControlState.normal)
+        
+        let width = lapTableBtn.superview!.frame.width / 5
+        
+        lapTableBtn.snp.makeConstraints { make in
+            make.width.equalTo(width)
+            make.height.equalTo(lapTableBtn.snp.width)
+            make.bottom.equalTo(lapTableBtn.superview!).offset(-width - CGFloat(Constants.defaultMargin))
+            make.right.equalTo(lapTableBtn.superview!).offset(-width - CGFloat(Constants.defaultMargin / 2))
+        }
+        
+        lapTableBtn.layer.borderWidth = 2.0
+        
+        lapTableBtn.layoutIfNeeded()
+        
+        lapTableBtn.layer.cornerRadius = lapTableBtn.frame.size.height / 2
     }
     
     func onStartTap() {
